782f52befa8af2f588763e7d20a1b50d06cf25e7
(Student) - Denis Bastien Becle <c00254311@ITCARLOW.IE>
2020-01-13 10:33:37 +0000

    down face

--- SFMLOpenGL/Game.cpp
+++ SFMLOpenGL/Game.cpp
@@ -15,24 +15,30 @@ Game::~Game() {}
 // A : -1, 1 -5
 // B : 1, 1, -5 
 // C : 1, -1, -5
-// D : -1, -1 , -5
+// D : -1, -1, -5
 //BACK 
-// E : -1, 1 -6
-// F : 1, 1, -6
-// G : 1, -1, -6
-// H : -1, -1 , -6
+// E : -1, 1, -7
+// F : 1, 1, -7
+// G : 1, -1, -7
+// H : -1, -1, -7
 
 // Vertices
-float vertices[] = { 1.0f, 1.0f, -6.0f, //T1 EFG
-					-1.0f, 1.0f, -6.0f,
-					-1.0f, -1.0f, -6.0f,
-					 1.0f, 1.0f, -6.0f, //T2 FGH
-					 1.0f, -1.0f, -6.0f,
-					-1.0f, -1.0f, -6.0f,
-					1.0f, 1.0f, -5.0f, //T3 ABC
+float vertices[] = { 1.0f, 1.0f, -7.0f, //T1 EFG
+					-1.0f, 1.0f, -7.0f,
+					-1.0f, -1.0f, -7.0f,
+					 1.0f, 1.0f, -7.0f, //T2 FGH
+					 1.0f, -1.0f, -7.0f,
+					-1.0f, -1.0f, -7.0f,
+					 1.0f, -1.0f, -5.0f, //T3 CDG
+					-1.0f, -1.0f, -5.0f,
+					 1.0f, -1.0f, -7.0f,
+					-1.0f, -1.0f, -5.0f, //T4 DGH
+					 1.0f, -1.0f, -7.0f,
+					-1.0f, -1.0f, -7.0f,
+					 1.0f, 1.0f, -5.0f, //T5 ABC
 					-1.0f, 1.0f, -5.0f,
 					-1.0f, -1.0f, -5.0f,
-					 1.0f, 1.0f, -5.0f, //T4 BCD
+					 1.0f, 1.0f, -5.0f, //T6 BCD
 					 1.0f, -1.0f, -5.0f,
 					-1.0f, -1.0f, -5.0f 
 };
@@ -44,16 +50,22 @@ float colors[] = {  0.0f, 0.0f, 0.8f, //T1
 					0.0f, 0.0f, 0.6f, //T2
 					0.0f, 0.0f, 0.3f,
 					0.0f, 0.0f, 0.6f,
-					0.8f, 0.0f, 0.0f, //T3 
+					0.0f, 1.0f, 0.0f, //T3 
+					0.0f, 0.8f, 0.0f,
+					0.0f, 1.0f, 0.0f,
+					0.0f, 0.6f, 0.0f, //T4
+					0.0f, 0.3f, 0.0f,
+					0.0f, 0.6f, 0.0f,
+					0.8f, 0.0f, 0.0f, //T5 
 					1.0f, 0.0f, 0.0f,
 					0.8f, 0.0f, 0.0f,
-					0.6f, 0.0f, 0.0f, //T4
+					0.6f, 0.0f, 0.0f, //T6
 					0.3f, 0.0f, 0.0f,
 					0.6f, 0.0f, 0.0f,
 };
 
 // Index to be drawn
-unsigned int vertex_index[] = { 0,1,2,3,4,5,6,7,8,9,10,11 };
+unsigned int vertex_index[] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17 };
 
 void Game::run()
 {
@@ -110,7 +122,7 @@ void Game::render()
 
 	//glDrawArrays(GL_TRIANGLES, 0, 3);
 
-	glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, &vertex_index);
+	glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, &vertex_index);
 
 	glDisableClientState(GL_COLOR_ARRAY);
 	glDisableClientState(GL_VERTEX_ARRAY);
